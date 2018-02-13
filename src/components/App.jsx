b1048f5c124808adf003ff66756035f02e3f8c3c
Oksana Tkachenko <oksana@crystax.net>
2018-02-13 21:39:26 +0200

    changed navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import {
   Route
 } from 'react-router-dom';
 
-//import Header from './Header.jsx';
-const Header = () => <h1>Header</h1>;
+import Header from './Header.jsx';
+//const Header = () => <h1>Header</h1>;
 const MainPage = () => <h1>MainPage</h1>
 const Review = () => <h1>Review</h1>
 const Footer = () => <h1>Footer</h1>
