fa9b0d48dfcbbd60a0dc08fc878d315581d957f2
Oksana Tkachenko <oksana@crystax.net>
2018-02-13 19:01:43 +0200

    added react

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from index.js');
-});
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 
